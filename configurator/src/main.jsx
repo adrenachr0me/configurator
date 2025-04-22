94f7b5db513871c699c2c57d9ed66a6c3e0dc5b4
adrenachr0me <telepyzix@gmail.com>
2025-04-23 01:25:46 +0200

    b2b
    added router(not uploaded db(db created))

--- configurator/src/main.jsx
+++ configurator/src/main.jsx
@@ -7,6 +7,14 @@ import Footer from "./components/footer";
 import "./index.css";
 import CartSite from "./components/cart-site";
 import Menu from "./components/menu";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useParams,
+  useNavigate,
+} from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
