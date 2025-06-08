06d48d276865e5ef4ea017b7950d58909bb96446
adrenachr0me <telepyzix@gmail.com>
2025-06-09 01:20:39 +0200

    step to host

--- configurator/backend/index.js
+++ configurator/backend/index.js
@@ -1,46 +1,53 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/db");
 
 dotenv.config();
 const app = express();
 
+
 connectDB();
+
 
 app.use(cors());
 app.use(express.json());
+
+
 const userRoutes = require("./routes/userRoutes");
-console.log(userRoutes);
-app.use("/api/users", userRoutes);
-
 const msgRoutes = require("./routes/msgRoutes");
-app.use("/api/messages", msgRoutes);
-
 const gpuRoutes = require("./routes/gpuRoutes");
-app.use("/api/gpus", gpuRoutes);
-
 const cpuRoutes = require("./routes/cpuRoutes");
-app.use("/api/cpus", cpuRoutes);
-
 const ramRoutes = require("./routes/ramRoutes");
-app.use("/api/rams", ramRoutes);
-
 const strRoutes = require("./routes/strRoutes");
-app.use("/api/storage", strRoutes);
 const pbsRoutes = require("./routes/pbsRoutes");
-app.use("/api/power", pbsRoutes);
 const mbsRoutes = require("./routes/mbsRoutes");
-app.use("/api/motherboards", mbsRoutes);
 const coolsRoutes = require("./routes/coolsRoutes");
-app.use("/api/cooler", coolsRoutes);
 const caseRoutes = require("./routes/caseRoutes");
-app.use("/api/cases", caseRoutes);
 const prebuildRoutes = require("./routes/prebuildRoutes");
+
+app.use("/api/users", userRoutes);
+app.use("/api/messages", msgRoutes);
+app.use("/api/gpus", gpuRoutes);
+app.use("/api/cpus", cpuRoutes);
+app.use("/api/rams", ramRoutes);
+app.use("/api/storage", strRoutes);
+app.use("/api/power", pbsRoutes);
+app.use("/api/motherboards", mbsRoutes);
+app.use("/api/cooler", coolsRoutes);
+app.use("/api/cases", caseRoutes);
 app.use("/api/prebuilds", prebuildRoutes);
-app.get("/", (req, res) => {
-  res.send("Сервер работает");
+
+
+const frontendPath = path.join(__dirname, "../dist");
+app.use(express.static(frontendPath));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
